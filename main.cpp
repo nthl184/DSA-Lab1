fc76d6b603f803c3daee2f66bbf7556ca4be818a
nthl184 <2004nthl@gmail.com>
2024-09-25 15:23:48 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+
+using namespace std;
+class node
+{public:
+    int data;
+    node *next;
+
+    node(int x)
+    {
+        data = x;
+        next = nullptr;
+    }
+};
+class LinkedList
+{public:
+    node *Head = nullptr;
+    node *Tail = nullptr;
+};
+void printList(LinkedList l)
+{
+    if(l.Head == nullptr) cout<<"Linked List is empty."<<'\n';
+    else
+    {
+        node *p = l.Head;
+        cout<<"The Linked List is: ";
+        while(p!=nullptr)
+        {
+            cout<<p->data<<" ";
+            p = p->next;
+        }
+        cout<<'\n';
+    }
+}
+//Insert a Node to LinkedList
+void addNode(LinkedList &l, int x) //add a Node with data x (add to tail of list)
+{
+    node *newNode = new node(x);
+    if(l.Head == nullptr)
+    {
+        l.Head = l.Tail = newNode;
+        return;
+    }
+    l.Tail->next = newNode;
+    l.Tail = newNode;
+}
+int elementNode(LinkedList l) //return the number element of LinkedList
+{
+    int cnt = 1;
+    node *tmp=l.Head;
+    while(tmp->next != nullptr)
+    {
+        cnt++;
+        tmp = tmp->next;
+    }
+    return cnt;
+}
+
+//Detect a Cycle in a Linked List
+bool hasCycle(LinkedList l)
+{
+    if(l.Head == nullptr || l.Head->next == nullptr) //the empty list or one node is not cycle
+        return false;
+    node *fast = l.Head;
+    node *slow = l.Head;
+    while(fast != nullptr && fast->next != nullptr)
+    {
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast)
+            return true; //has Cycle
+    }
+    return false; //not cycle
+}
+int main()
+{
+    freopen("exer.inp","r",stdin);
+    LinkedList l;
+    int x;
+    cout<<"Enter the data element until enter 0: \n";
+    cin>>x;
+    while(x!=0)
+    {
+        addNode(l,x);
+        cin>>x;
+    }
+    if(hasCycle(l)) cout<<"The LinkedList has cycle. \n";
+    else cout<<"The LinkedList has not cycle. \n";
+    return 0;
+}
